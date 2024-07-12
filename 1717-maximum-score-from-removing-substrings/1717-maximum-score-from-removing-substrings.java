9be8f0d12e695fc4c9d6276796bb18f355d35f0e
Pankaj Dewhare <96539244+PankuD002@users.noreply.github.com>
2024-07-12 16:53:15 +0530

    Time: 159 ms (56.57%), Space: 46.7 MB (39.39%) - LeetHub

---
+++ 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int maximumGain(String s, int x, int y) {
+        char first = x>y ?'a':'b';
+        char second =x>y ? 'b':'a';
+        int min =Math.min(x,y);
+        int max =Math.max(x,y);
+        int ans=0;
+        
+        Stack<Character> stack =new Stack<>();
+        for(char ch: s.toCharArray()){
+            if(!stack.isEmpty() && stack.peek()==first && ch==second){
+                stack.pop();
+                ans+=max;
+            }
+            else{
+                stack.push(ch);
+            }
+        }
+        Stack<Character> stack2 = new Stack<>();
+        while(!stack.isEmpty()){
+            char ch =stack.pop();
+            if(!stack2.isEmpty() && stack2.peek()==first && ch==second){
+                stack2.pop();
+                ans+=min;
+            }
+            else{
+                stack2.push(ch);
+            }
+        }
+        return ans;
+    }
+}
